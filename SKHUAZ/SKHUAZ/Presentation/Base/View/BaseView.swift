26013ddcd5e69ad74eb8d9c33a16ab73fd55a26f
Cheon, Seongwoo <cjs1399@naver.com>
2023-08-16 15:47:35 +0900

    [Setting] #1 - 프로젝트 폴더링 및 파일링

---
+++ SKHUAZ/SKHUAZ/Presentation/Base/View/BaseView.swift
@@ -0,0 +1,48 @@
+//
+//  BaseView.swift
+//  SKHUAZ
+//
+//  Created by 천성우 on 2023/08/16.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class BaseView: UIView {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Components Property
+    
+    private func setUI(){
+    }
+    
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {}
+    
+    // MARK: - Methods
+    
+    private func setAddTarget() {}
+    
+    // MARK: - @objc Methods
+    
+}
