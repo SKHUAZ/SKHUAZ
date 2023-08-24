fd70a4b94f1006741d7295b6a5b993f440520793
Cheon, Seongwoo <cjs1399@naver.com>
2023-08-24 18:14:45 +0900

    [Feature] #4 - 데이터 모델 수정

--- SKHUAZ/SKHUAZ/Presentation/Evaluate/DataModel/EvaluateDataModel.swift
+++ SKHUAZ/SKHUAZ/Presentation/Evaluate/DataModel/EvaluateDataModel.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-
 struct EvaluateDataModel {
+    var department: String
     var lectureNameLabel: String
     var professorNameLabel: String
     var courseYearLabel: String
@@ -17,20 +17,23 @@ struct EvaluateDataModel {
     var dateCreatedLabel: String
 }
 
-let review1 = EvaluateDataModel(lectureNameLabel: "자바 프로그래밍", professorNameLabel: "문성현",
-                                courseYearLabel: "2023-1", totalScoreLabel: 4,
-                                authorNameLabel: "김철수", dateCreatedLabel: "2023-08-17")
-let review2 = EvaluateDataModel(lectureNameLabel: "데이터베이스", professorNameLabel: "홍은지",
-                                courseYearLabel: "2023-1", totalScoreLabel: 5,
-                                authorNameLabel: "박지민", dateCreatedLabel: "2023-08-18")
-let review3 = EvaluateDataModel(lectureNameLabel: "알고리즘", professorNameLabel: "노은하",
-                                courseYearLabel: "2022-2", totalScoreLabel: 3,
-                                authorNameLabel: "이영호", dateCreatedLabel: "2023-08-19")
-let review4 = EvaluateDataModel(lectureNameLabel: "자료구조", professorNameLabel: "노은하",
-                                courseYearLabel: "2023-1", totalScoreLabel: 4,
-                                authorNameLabel: "홍길동", dateCreatedLabel: "2023-08-20")
-let review5 = EvaluateDataModel(lectureNameLabel: "모바일프로그래밍", professorNameLabel: "이승진",
-                                courseYearLabel: "2022-1", totalScoreLabel: 5,
-                                authorNameLabel: "정재민", dateCreatedLabel: "2023-08-21")
+let review1 = EvaluateDataModel(department: "소프트웨어공학", lectureNameLabel: "자바 프로그래밍", professorNameLabel: "문성현",
+                                courseYearLabel: "2023-1", totalScoreLabel: 4, authorNameLabel: "김철수",
+                                dateCreatedLabel: "2023-08-17")
+let review2 = EvaluateDataModel(department: "소프트웨어공학", lectureNameLabel: "데이터베이스", professorNameLabel: "홍은지",
+                                courseYearLabel: "2023-1", totalScoreLabel: 5, authorNameLabel: "박지민",
+                                dateCreatedLabel: "2023-08-18")
+let review3 = EvaluateDataModel(department: "소프트웨어공학", lectureNameLabel: "알고리즘", professorNameLabel: "노은하",
+                                courseYearLabel: "2022-2", totalScoreLabel: 3, authorNameLabel: "이영호",
+                                dateCreatedLabel: "2023-08-19")
+let review4 = EvaluateDataModel(department: "소프트웨어공학", lectureNameLabel: "자료구조", professorNameLabel: "노은하",
+                                courseYearLabel: "2023-1", totalScoreLabel: 4, authorNameLabel: "홍길동",
+                                dateCreatedLabel: "2023-08-20")
+let review5 = EvaluateDataModel(department: "소프트웨어공학", lectureNameLabel: "모바일프로그래밍", professorNameLabel: "이승진",
+                                courseYearLabel: "2022-1", totalScoreLabel: 5, authorNameLabel: "정재민",
+                                dateCreatedLabel: "2023-08-21")
+let review6 = EvaluateDataModel(department: "정보통신공학", lectureNameLabel: "리눅스 시스템", professorNameLabel: "박정식",
+                                courseYearLabel: "2023-1", totalScoreLabel: 3, authorNameLabel: "천성우",
+                                dateCreatedLabel: "2023-07-23")
 
-var reviewList: [EvaluateDataModel] = [review1, review2, review3, review4, review5]
+var reviewList: [EvaluateDataModel] = [review1, review2, review3, review4, review5, review6]
