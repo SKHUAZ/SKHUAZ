999922607e6104ae34e48381ae16c40d41d30cb1
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-05 00:00:46 +0900

    [Feature] #4 - CreateEvaluateView 초기 작업

--- SKHUAZ/SKHUAZ/Presentation/Evaluate/View/CreateEvaluateView.swift
+++ SKHUAZ/SKHUAZ/Presentation/Evaluate/View/CreateEvaluateView.swift
@@ -17,7 +17,7 @@ class CreateEvaluateView: UIView {
     
     // MARK: - UI Components
     
-    private let mainImage = UIView()
+    private let mainImage = UIImageView()
     private let mainContainer = UIView()
     private let topSpace = UIView()
     private let topLabel = UILabel()
@@ -29,6 +29,8 @@ class CreateEvaluateView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUI()
+        setLayout()
         
     }
     
@@ -60,13 +62,67 @@ extension CreateEvaluateView {
             $0.textColor = .white
             $0.font = .systemFont(ofSize: 16)
         }
+        
+        cancelButton.do {
+            $0.setTitle("목록", for: .normal)
+            $0.setTitleColor(UIColor.black, for: .normal)
+            $0.titleLabel?.font = .systemFont(ofSize: 8)
+            $0.backgroundColor = .white
+            $0.layer.cornerRadius = 6
+            $0.layer.borderColor = UIColor.black.cgColor
+            $0.layer.borderWidth = 1
+        }
+        
+        saveButton.do {
+            $0.setTitle("저장", for: .normal)
+            $0.titleLabel?.font = .systemFont(ofSize: 8)
+            $0.setTitleColor(UIColor.white, for: .normal)
+            $0.backgroundColor = .black
+            $0.layer.cornerRadius = 6
+            $0.layer.borderColor = UIColor.black.cgColor
+            $0.layer.borderWidth = 1
+        }
     }
     
     
     // MARK: - Layout Helper
     
     private func setLayout() {
+        addSubviews(mainContainer, topSpace,
+                    cancelButton, saveButton)
+        topSpace.addSubviews(topLabel)
         
+        topLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(9)
+        }
+        
+        mainContainer.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.leading.equalToSuperview()
+            $0.height.equalTo(600)
+        }
+        
+        topSpace.snp.makeConstraints {
+            $0.top.equalTo(mainContainer.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(37)
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.top.equalTo(mainContainer.snp.bottom).offset(41)
+            $0.width.equalTo(77)
+            $0.height.equalTo(30)
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.top.equalTo(mainContainer.snp.bottom).offset(41)
+            $0.width.equalTo(77)
+            $0.height.equalTo(30)
+
+        }
     }
     
     // MARK: - Methods
--- SKHUAZ/SKHUAZ/Presentation/Evaluate/ViewController/CreateEvaluateViewController.swift
+++ SKHUAZ/SKHUAZ/Presentation/Evaluate/ViewController/CreateEvaluateViewController.swift
@@ -11,37 +11,60 @@ import SnapKit
 import Then
 
 final class CreateEvaluateViewController: UIViewController {
-    
+
     // MARK: - UI Components
     
+    private let mainLogo = UIImageView()
+    private let createEvaluateView = CreateEvaluateView()
+
     // MARK: - Properties
-    
+
     // MARK: - Initializer
-    
+
     // MARK: - View Life Cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(false, animated: true)
+
         setUI()
         setLayout()
     }
 }
 
 extension CreateEvaluateViewController {
-    
+
     // MARK: - UI Components Property
-    
+
     private func setUI() {
+        view.backgroundColor = .white
         
+        mainLogo.do {
+            $0.contentMode = .scaleAspectFit
+            $0.image = Image.Logo3
+        }
     }
-    
+
     // MARK: - Layout Helper
-    
+
     private func setLayout() {
+        view.addSubviews(mainLogo, createEvaluateView)
+        
+        mainLogo.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(47)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(43)
+            $0.width.equalTo(168)
+        }
+        
+        createEvaluateView.snp.makeConstraints {
+            $0.top.equalTo(mainLogo.snp.bottom).offset(24)
+            $0.leading.trailing.equalToSuperview().inset(39)
+        }
         
     }
-    
+
     // MARK: - Methods
-    
+
     // MARK: - @objc Methods
 }
