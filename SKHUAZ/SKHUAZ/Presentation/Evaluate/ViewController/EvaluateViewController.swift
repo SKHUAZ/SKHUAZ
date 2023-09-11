279e84c2ed7a163deddaac6a24cb28e433a20822
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-11 10:58:05 +0900

    [Feature] #10 - extension 추가로 코드 부분 수정

--- SKHUAZ/SKHUAZ/Presentation/Evaluate/ViewController/EvaluateViewController.swift
+++ SKHUAZ/SKHUAZ/Presentation/Evaluate/ViewController/EvaluateViewController.swift
@@ -10,60 +10,46 @@ import UIKit
 import SnapKit
 import Then
 
-final class EvaluateViewController: UIViewController {
+class EvaluateViewController: UIViewController {
     
-    // MARK: - UI Components
+    //MARK: - UI Components
     
-    private let mainImage = UIImageView()
+    private let mainLogo = UIImageView()
     private let titleLabel = UILabel()
     private let searchTextField = UITextField()
-    private let evaluateListView = UITableView()
+    private lazy var tableView = UITableView(frame:.zero, style:.plain)
     private let createButton = UIButton()
     private let wroteMeButton = UIButton()
     
-    // MARK: - Properties
+    //MARK: - Properties
     
     private var wroteMeButtonBottomConstraint: Constraint?
     private var isMove: Bool = false
-    var reviewList: [EvaluateDataModel] = []
-    var filteredReviewList: [EvaluateDataModel] = []
-    var isFiltering = false
-    
-    // MARK: - Initializer
-    
-    init() {
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - View Life Cycle
+    private var isTouch: Bool = false
+    private var filteredReviews: [EvaluateDataModel]!
+    var reviews: [EvaluateDataModel]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.setNavigationBarHidden(true, animated: true)
         
+        setupData()
         setUI()
         setLayout()
-        setupData()
+        setRegister()
         setDelegate()
-        setTapScreen()
         addTarget()
-        setUITableView()
     }
 }
 
 extension EvaluateViewController {
     
     // MARK: - UI Components Property
-    
+
     private func setUI() {
         
         view.backgroundColor = .white
         
-        mainImage.do {
+        mainLogo.do {
             $0.contentMode = .scaleAspectFit
             $0.image = Image.Logo1
         }
@@ -78,15 +64,12 @@ extension EvaluateViewController {
             $0.placeholder = "검색어를 입력해주세요"
             $0.font = .systemFont(ofSize: 8)
             $0.backgroundColor = UIColor(red: 0.937, green: 0.937, blue: 0.937, alpha: 1)
-            
             $0.borderStyle = .roundedRect
-            $0.addTarget(self, action: #selector(searchTextChanged), for: .editingChanged)
         }
         
-        evaluateListView.do {
-            $0.separatorStyle = .none
+        tableView.do {
             $0.showsVerticalScrollIndicator = false
-            $0.register(EvaluateTableViewCell.self, forCellReuseIdentifier: "Cell")
+            $0.separatorStyle = .none
         }
         
         createButton.do {
@@ -99,36 +82,32 @@ extension EvaluateViewController {
     }
     
     // MARK: - Layout Helper
-    
+
     private func setLayout() {
-        view.addSubviews(mainImage, titleLabel,searchTextField,
-                         evaluateListView, wroteMeButton, createButton)
         
-        mainImage.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
+        view.addSubviews(mainLogo, titleLabel, searchTextField, tableView, wroteMeButton, createButton)
+        
+        mainLogo.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(47)
             $0.centerX.equalToSuperview()
             $0.height.equalTo(43)
             $0.width.equalTo(168)
-            
         }
         
         titleLabel.snp.makeConstraints {
-            $0.top.equalTo(mainImage.snp.bottom)
+            $0.top.equalTo(mainLogo.snp.bottom)
             $0.centerX.equalToSuperview()
         }
         
-        
         searchTextField.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(4)
-            $0.leading.equalToSuperview().offset(30)
-            $0.trailing.equalToSuperview().inset(30)
+            $0.leading.trailing.equalToSuperview().inset(39)
             $0.height.equalTo(30)
         }
         
-        evaluateListView.snp.makeConstraints {
-            $0.top.equalTo(searchTextField.snp.bottom).offset(10)
-        
-            $0.leading.trailing.bottom.equalToSuperview()
+        tableView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(searchTextField.snp.bottom).offset(16)
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
         
         createButton.snp.makeConstraints {
@@ -144,34 +123,45 @@ extension EvaluateViewController {
         }
     }
     
-    // MARK: - Add
-    
-    private func addTarget() {
+    // MARK: - Methods
 
-        createButton.addTarget(self, action: #selector(movewroteMeButton), for: .touchUpInside)
-        wroteMeButton.addTarget(self, action: #selector(wroteMeButtonTapped), for: .touchUpInside)
+    private func setupData() {
+        reviews = [review1, review2, review3, review4, review5, review6]
+        filteredReviews = reviews
     }
     
-    // MARK: - Methods
-    
-    private func setupData() {
-        reviewList = [review1, review2, review3, review4, review5, review6]
-        filteredReviewList = reviewList
-        evaluateListView.reloadData()
+    private func setRegister() {
+        tableView.register(EvaluateTableViewCell.self,
+                           forCellReuseIdentifier:"evaluateCell")
     }
     
     private func setDelegate() {
-        evaluateListView.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchTextField.delegate = self
+
     }
     
-    private func setUITableView() {
-        evaluateListView.dataSource = self
+    private func addTarget() {
+        createButton.addTarget(self, action: #selector(movewroteMeButton), for: .touchUpInside)
+        searchTextField.addTarget(self, action: #selector(searchTextChanged), for: .editingChanged)
+        wroteMeButton.addTarget(self, action: #selector(wroteMeButtonTapped), for: .touchUpInside)
     }
+    
+    // MARK: - @objc Methods
     
     @objc
     private func wroteMeButtonTapped() {
-        searchTextField.text = "천성우"
-        searchTextChanged()
+        isTouch.toggle()
+        if isTouch {
+            wroteMeButton.setImage(Image.WritingOn, for: .normal)
+            searchTextField.text = "천성우"
+            searchTextChanged()
+        } else {
+            wroteMeButton.setImage(Image.WritingOff, for: .normal)
+            searchTextField.text = ""
+            searchTextChanged()
+        }
     }
     
     @objc
@@ -192,38 +182,70 @@ extension EvaluateViewController {
             }
         }
         print(isMove)
-        
     }
     
     @objc
-    func searchTextChanged() {
-        if let searchText = searchTextField.text?.lowercased(), !searchText.isEmpty {
-            filteredReviewList = reviewList.filter { ($0.department.lowercased().contains(searchText) || $0.lectureNameLabel.lowercased().contains(searchText) || $0.title.lowercased().contains(searchText) || $0.authorNameLabel.lowercased().contains(searchText) || $0.professorNameLabel.lowercased().contains(searchText)) }
-            isFiltering = true
-        } else {
-            filteredReviewList = reviewList
-            isFiltering = false
+    private func searchTextChanged() {
+        
+        guard let searchText = searchTextField.text, !searchText.isEmpty else {
+            filteredReviews = reviews
+            tableView.reloadData()
+            return
         }
-        evaluateListView.reloadData()
+        
+        filteredReviews = reviews.filter { review in
+            return review.department.contains(searchText) ||
+            review.lectureNameLabel.contains(searchText) ||
+            review.title.contains(searchText) ||
+            review.authorNameLabel.contains(searchText) ||
+            review.professorNameLabel.contains(searchText)
+        }
+        
+        tableView.reloadData()
     }
 }
 
-extension EvaluateViewController: UITableViewDataSource, UITableViewDelegate {
+extension EvaluateViewController: UITextFieldDelegate{
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+        
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        tableView.isUserInteractionEnabled = false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        tableView.isUserInteractionEnabled = true
+    }
+}
+
+extension EvaluateViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 170
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredReviewList.count
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+       return filteredReviews.count
+    }
+
+    func tableView(_ tableView:UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier:"evaluateCell",for:indexPath) as! EvaluateTableViewCell
+       let review = filteredReviews[indexPath.row]
+       cell.configure(with:review)
+
+       return cell
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EvaluateTableViewCell
-        let review = filteredReviewList[indexPath.row]
+    
+    
+    func tableView(_ tableview:UITableView,didSelectRowAt indexPath:IndexPath){
+        print("You selected cell #\(reviews[indexPath.row].title)")
+        let detailVC = DetailEvaluateViewController()
+        // 필요한 경우 detailVC의 데이터를 설정할 수 있
+        // ex) detailVC.dataModel = dataModels[indexPath.row]
         
-        cell.configure(with: review)
-        
-        return cell
+        self.navigationController?.pushViewController(detailVC, animated: true)
     }
 }
