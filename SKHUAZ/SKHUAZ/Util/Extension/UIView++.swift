8e2da303eb4804f254e05793d67193166ffcb579
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-13 22:10:02 +0900

    [Feature] #14 - edit UIView extension

--- SKHUAZ/SKHUAZ/Util/Extension/UIView++.swift
+++ SKHUAZ/SKHUAZ/Util/Extension/UIView++.swift
@@ -13,4 +13,19 @@ extension UIView {
             self.addSubview($0)
         }
     }
+    
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+    
+//    뷰의 왼쪽 상단 모서리
+//   public static var layerMinXMinYCorner: CACornerMask { get }
+//    뷰의 오른쪽 상단 모서리
+//    public static var layerMaxXMinYCorner: CACornerMask { get }
+//    뷰의 왼쪽 하단 모서리
+//    public static var layerMinXMaxYCorner: CACornerMask { get }
+//    뷰의 오른쪽 하단 모서리
+//    public static var layerMaxXMaxYCorner: CACornerMask { get }
 }
