0bfce8424284ea42389b798bb921c6a59a4167e0
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-14 17:13:17 +0900

    [Feature] #14 - 서버 대응 선수과목 더미 데이터 구현

---
+++ SKHUAZ/SKHUAZ/Presentation/Essential/DataModel/EssentialDataModel.swift
@@ -0,0 +1,21 @@
+//
+//  EssentialDataModel.swift
+//  SKHUAZ
+//
+//  Created by 천성우 on 2023/09/14.
+//
+
+import Foundation
+
+struct EssentialDataModel {
+    var lectureName: String
+    var professorName: String
+}
+
+
+let essentialDataModels: [EssentialDataModel] = [
+    EssentialDataModel(lectureName: "Java프로그래밍", professorName: "문성현"),
+    EssentialDataModel(lectureName: "웹개발입문", professorName: "박신영"),
+    EssentialDataModel(lectureName: "UIkit 입문", professorName: "천성우"),
+    EssentialDataModel(lectureName: "서버구축기본", professorName: "허지영")
+]
