b1d54119ac4ffa3c0279da4b4fc331a7323f525f
inhomun <inho8063@icloud.com>
2023-09-21 01:55:32 +0900

    [Feat] #20 SignUpView 구현 완료

--- SKHUAZ/SKHUAZ/Application/SceneDelegate.swift
+++ SKHUAZ/SKHUAZ/Application/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        let vc = TabBarController()
+        let vc = SignUpViewController()
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
