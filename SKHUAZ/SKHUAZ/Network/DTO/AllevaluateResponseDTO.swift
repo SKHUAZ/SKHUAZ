49359a69e09c6c535af78b703ecb792e3bdc255e
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-23 13:19:17 +0900

    [Feature] #18 - Response Body 대응 DTO 생성

---
+++ SKHUAZ/SKHUAZ/Network/DTO/AllevaluateResponseDTO.swift
@@ -0,0 +1,39 @@
+//
+//  AllevaluateResponseDTO.swift
+//  SKHUAZ
+//
+//  Created by 천성우 on 2023/09/22.
+//
+
+import Foundation
+
+// MARK: - Template
+struct AllevaluateResponseDTO: Codable {
+    let statusCode: Int
+    let message: String
+    let data: [AllevaluateData]
+}
+
+// MARK: - Datum
+struct AllevaluateData: Codable {
+    let evaluationID, teamPlay, task, practice: Int
+    let presentation: Int
+    let title, review, createdAt, email: String
+    let lecture: Lecture
+
+    enum CodingKeys: String, CodingKey {
+        case evaluationID = "evaluationId"
+        case teamPlay, task, practice, presentation, title, review, createdAt, email, lecture
+    }
+}
+
+// MARK: - Lecture
+struct Lecture: Codable {
+    let lectureID: Int
+    let deptName, lecName, profName, semester: String
+
+    enum CodingKeys: String, CodingKey {
+        case lectureID = "lectureId"
+        case deptName, lecName, profName, semester
+    }
+}
