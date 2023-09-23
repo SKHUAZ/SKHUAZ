62eea3b79b4a571392796512b2f799ab48084ba7
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-23 13:18:43 +0900

    [Feature] #18 - Alamofire 대비 Foundation, Config 코드 작성

--- SKHUAZ/SKHUAZ/Network/Foundation/URLConst.swift
+++ SKHUAZ/SKHUAZ/Network/Foundation/URLConst.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct APIConstants {
+    static let contentType = "Content-Type"
+    static let applicationJSON = "application/json"
+}
+
+extension APIConstants {
+    
+    static var defaultHeader: Dictionary<String,String> {
+        [contentType: applicationJSON]
+    }
+}
