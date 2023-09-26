1c4c5fa3b4a2774dd4da03843d4a0b1eeb0cefc4
ParkSY0919 <qhr498@gmail.com>
2023-09-26 16:57:27 +0900

    [Feature] # 23 - RootRecommendAPI 생성 및 전체 루트추천글 조회 기능 추가

---
+++ SKHUAZ/SKHUAZ/Network/API/RootRecommend/RootRecommendAPI.swift
@@ -0,0 +1,36 @@
+//
+//  RootRecommendAPI.swift
+//  SKHUAZ
+//
+//  Created by 박신영 on 2023/09/25.
+//
+
+import Foundation
+import Alamofire
+
+final class RootRecommendAPI: BaseAPI {
+    static let shared = RootRecommendAPI()
+    
+    private override init() {}
+}
+
+extension RootRecommendAPI {
+    
+    // MARK: - 전체 루트추천 조회
+    
+    public func getAllRootRecommend(token: String, completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(RootRecommendRouter.getAllRootRecommend(token: token)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: AllRootRecommendResponseDTO.self,
+                                completion: completion)
+        }
+    }
+    
+    // MARK: - 루트추천 생성
+    
+    public func createRootRecommend(token: String, completion: @escaping(NetworkResult<Any>) -> Void) {
+        
+    }
+    
+    
+}
