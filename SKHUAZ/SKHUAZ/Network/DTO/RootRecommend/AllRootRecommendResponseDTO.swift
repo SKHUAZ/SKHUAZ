a113ac0b5db10776ffb6d23cfdd1772fe69cbbca
ParkSY0919 <qhr498@gmail.com>
2023-09-27 02:50:49 +0900

    [Feature] # 38 - 모든강의평 불러오기, 강의평 생성 DTO 추가

---
+++ SKHUAZ/SKHUAZ/Network/DTO/RootRecommend/AllRootRecommendResponseDTO.swift
@@ -0,0 +1,8 @@
+//
+//  AllRootRecommendResponseDTO.swift
+//  SKHUAZ
+//
+//  Created by 박신영 on 2023/09/27.
+//
+
+import Foundation
---
+++ SKHUAZ/SKHUAZ/Network/DTO/RootRecommend/CreateRootRecommendDTO.swift
@@ -0,0 +1,8 @@
+//
+//  CreateRootRecommendDTO.swift
+//  SKHUAZ
+//
+//  Created by 박신영 on 2023/09/27.
+//
+
+import Foundation
