e899c838499a8419d7eaecf9898e205027f55796
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-26 13:45:13 +0900

    [Feature] #29 - 서버통신 기본 셋팅 Fix

--- SKHUAZ/SKHUAZ/Util/NameSpace/Literals.swift
+++ SKHUAZ/SKHUAZ/Util/NameSpace/Literals.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJhdWQiOiJnandsZHVkMDcxOUBuYXZlci5jb20iLCJpYXQiOjE2OTU3MDAyNDUsImV4cCI6MTY5NjA2MDI0NX0.Ua4nWSqGekDi3uGk-zq31guD2VhP6R_TAwqkjY8B6LYjINs2ictFoIKlr8Gf6p8AjNTZQmlrw-DsLfeW3W_X7w"
+
