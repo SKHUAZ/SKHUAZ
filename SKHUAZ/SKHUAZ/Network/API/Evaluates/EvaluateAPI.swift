2b65764f14247d44243b3b39d23023c8af2915f1
Cheon, Seongwoo <cjs1399@naver.com>
2023-09-26 18:20:42 +0900

    [Fix] #33 - code Server Fix, Layout Fix

--- SKHUAZ/SKHUAZ/Network/API/Evaluates/EvaluateAPI.swift
+++ SKHUAZ/SKHUAZ/Network/API/Evaluates/EvaluateAPI.swift
@@ -16,7 +16,7 @@ final class EvaluateAPI: BaseAPI {
 }
 
 extension EvaluateAPI {
-    // 1. 전체 루틴 조회
+    // 1. 전체 강의평 조회
     
     public func getAllEvaluate(token: String, completion: @escaping(NetworkResult<Any>) -> Void) {
         AFManager.request(EvaluateRouter.getAllEvaluation(token: token)).responseData { response in
@@ -26,7 +26,7 @@ extension EvaluateAPI {
         }
     }
     
-    
+    // 2. 강의평 상세보기
     
     
     public func getDetailEvaluation(token: String,
@@ -51,9 +51,7 @@ extension EvaluateAPI {
                 }
     }
     
-    
-    
-    
+    // 3. 강의평 저장하기
     
     public func postCreateEvaluate(token: String,
                                    requestBody: CreateEvaluateRequestBody,
@@ -62,6 +60,19 @@ extension EvaluateAPI {
             self.disposeNetwork(response,
                                 dataModel: CreateEvaluateDTO.self,
                                 completion: completion)
+        }
+    }
+    
+    // 4. 강의평 삭제하기
+    
+    public func delEvaluate(token: String,
+                            evaluationId: Int,
+                            completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(EvaluateRouter.delEvaluation(token: token, evaluationId: evaluationId)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: DelEvaluateDTO.self,
+                                completion: completion)
+            
         }
     }
 }
