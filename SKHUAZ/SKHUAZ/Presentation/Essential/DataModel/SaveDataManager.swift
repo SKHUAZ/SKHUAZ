c3d66ce2e3771fac59689a3e30a925590ee974f4
Cheon, Seongwoo <cjs1399@naver.com>
2023-10-26 05:06:30 +0900

    [Feature] #43 - 싱글톤 패턴 추가

---
+++ SKHUAZ/SKHUAZ/Presentation/Essential/DataModel/SaveDataManager.swift
@@ -0,0 +1,14 @@
+//
+//  SaveDataManager.swift
+//  SKHUAZ
+//
+//  Created by 천성우 on 10/26/23.
+//
+
+import Foundation
+
+class SaveDataManager {
+    static let shared = SaveDataManager()
+    
+    var essentialData: [adminPreLecture] = []
+}
