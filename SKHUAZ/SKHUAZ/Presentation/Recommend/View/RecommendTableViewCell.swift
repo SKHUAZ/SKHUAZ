31081d0a9d30acab7b2e9fe0e16be3874c521cfe
Cheon, Seongwoo <cjs1399@naver.com>
2023-10-29 17:13:59 +0900

    [Fix] #51 - code Fix

--- SKHUAZ/SKHUAZ/Presentation/Recommend/View/RecommendTableViewCell.swift
+++ SKHUAZ/SKHUAZ/Presentation/Recommend/View/RecommendTableViewCell.swift
@@ -239,18 +239,24 @@ extension RecommendTableViewCell {
     }
     
     func configureUpdate(with review: RootRecommendDataModel, at indexPath: IndexPath) {
-        recommendTitle.text = review.title
-        recommendContent.text = review.recommendation
-        let creationDate = review.createAt.split(separator: "T")
-        creationDateLabel.text = String(creationDate[0])
-        
-        let lectureIndex = indexPath.row % (review.preLectures.count)
-        
-        majorNameLabel.text = review.preLectures[lectureIndex].lecNames[0]
-        
-        // 주어진 배열 범위 내에 있는지 확인합니다.
-        if lectureIndex + 1 < review.preLectures.count {
-            majorNameLabel2.text = review.preLectures[lectureIndex + 1].lecNames[0]
+            recommendTitle.text = review.title
+            recommendContent.text = review.recommendation
+            let creationDate = review.createAt.split(separator: "T")
+            creationDateLabel.text = String(creationDate[0])
+            
+            if review.preLectures.isEmpty {
+                majorNameLabel.text = "No lectures available"
+                majorNameLabel2.text = ""
+            } else {
+                // 주어진 배열 범위 내에 있는지 확인합니다.
+                let lectureIndex = indexPath.row % (review.preLectures.count)
+                majorNameLabel.text = review.preLectures[lectureIndex].lecNames[0]
+                
+                if lectureIndex + 1 < review.preLectures.count {
+                    majorNameLabel2.text = review.preLectures[lectureIndex + 1].lecNames[0]
+                } else {
+                    majorNameLabel2.text = "" // 다음 강의가 없는 경우 빈 문자열로 설정
+                }
+            }
         }
-    }
 }
